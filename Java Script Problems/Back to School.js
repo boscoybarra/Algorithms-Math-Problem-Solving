52893a8052ddfb862e241f7841ac0daa5619c60a
boscoybarra <bosco@ybarra.io>
2017-11-28 17:59:25 +0100

    new exercises on javascript from udacity

---
+++ Java Script Problems/Back to School.js
@@ -0,0 +1,25 @@
+/*
+ * Programming Quiz: Back to School
+ * Copyright: Udacity
+ * Directions:
+ * Write a switch statement to set the average salary of a person based on their type of completed education.
+
+ * Afterwards, print the following to the console.
+
+ * In 2015, a person with __________ earned an average of __________/year.
+ * Fill in the blanks with the type of education and the expected average salary. Make sure to use correct grammar in your printed statement. For help, refer to the findings above.
+
+ * In 2015, a person with a Bachelor's degree earned an average of $59,124/year.
+ * TIP: To print out the average salary with commas (i.e. 59,124), use the toLocaleString() method and pass it the locale "en-US". For example, salary.toLocaleString("en-US").
+ * TIP: Make sure to test your code with different values. For example,
+
+ * If education equals "an Associate's degree", then In 2015, a person with an Associate's degree earned an average of $41,496/year. should be printed to the console.
+ */
+
+// change the value of `education` to test your code
+var education = "no high school diploma";
+
+// set the value of this based on a person's education
+var salary;
+
+// your code goes here
