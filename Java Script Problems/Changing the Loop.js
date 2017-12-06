f07e5fbbbc8e0503373162bfe24d5ce8a59ec012
boscoybarra <bosco@ybarra.io>
2017-12-06 13:52:42 +0100

    new loop exercises

---
+++ Java Script Problems/Changing the Loop.js
@@ -0,0 +1,19 @@
+/*
+ * Programming Quiz: Changing the Loop
+ * Copyright Udacity
+ */
+
+// while loop
+/*
+var x = 9;
+while (x >= 1) {
+    console.log("hello " + x);
+    x = x - 1;
+}
+ */
+
+// for loop
+
+for (var x = 9; x >= 1; x--) {
+  console.log("hello " + x);
+}
