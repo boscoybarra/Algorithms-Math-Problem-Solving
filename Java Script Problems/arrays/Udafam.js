6389a8bb4d8209ea3b301b81f38dd4c8d61d3283
boscoybarra <bosco@ybarra.io>
2017-12-13 23:51:27 +0100

    new exercises

---
+++ Java Script Problems/arrays/Udafam.js
@@ -0,0 +1,16 @@
+/*
+ * Programming Quiz: UdaciFamily (6-1)
+ * Copyright Udacity
+ * Create an array called udaciFamily and add "Julia", "James", and your name to the array.
+ * Then, print the udaciFamily to the console using console.log.
+ */
+
+// Your Code
+
+var udaciFamily = []
+
+udaciFamily[0] = "Julia";
+udaciFamily[1] = "James";
+udaciFamily[2] = "Me";
+
+console.log(udaciFamily);
