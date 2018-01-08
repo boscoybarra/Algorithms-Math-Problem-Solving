3e7f90262037a7b7aa97f35ca633efe0b2f16ab8
boscoybarra <bosco@ybarra.io>
2018-01-08 21:51:12 +0100

    added and organized problem sets

---
+++ Java Script Problems/arrays/Quidditch/Quidditch.js
@@ -0,0 +1,9 @@
+/*
+* Programming Quiz: Quidditch Cup
+* Copyright Udacity
+* Directions:
+* Create a function called hasEnoughPlayers() that takes the team array as an argument and returns true or
+* false depending on if the array has at least seven players.
+ */
+
+// your code goes here
---
+++ Java Script Problems/arrays/Udafam/Udafam.js
@@ -0,0 +1,8 @@
+/*
+ * Programming Quiz: UdaciFamily (6-1)
+ * Copyright Udacity
+ * Create an array called udaciFamily and add "Julia", "James", and your name to the array.
+ * Then, print the udaciFamily to the console using console.log.
+ */
+
+// Your Code
